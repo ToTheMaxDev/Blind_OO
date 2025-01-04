25490306d9d5b04986288d4a76e80d616bdff62e
max <max@roosnl.com>
2025-01-04 23:26:55 +0100

    Add audio.o to .gitignore, rename main.cpp to audio.cpp.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-g++ -c main.cpp -I ./SFML-3.0.0/include
-g++ main.o -o main -L ./SFML-3.0.0/lib -lsfml-audio -lsfml-system
-export LD_LIBRARY_PATH=./SFML-3.0.0/lib && ./main
-
+g++ -c audio.cpp -I ./SFML-3.0.0/include
+g++ audio.o -o audio -L ./SFML-3.0.0/lib -lsfml-audio -lsfml-system
+export LD_LIBRARY_PATH=./SFML-3.0.0/lib && ./audio
